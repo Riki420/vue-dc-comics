f34cbf7c8e505874c0f4b3777a326585ac547fae
Riki420 <girottiriccardo1998@gmail.com>
2021-09-06 16:45:35 +0200

    Inizio Header e Inizio Main

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="header-top"></div>
+            <div class="header-bottom">
+                <figure>
+                    <img src="../assets/img/dc-logo.png" alt="">
+                </figure>
+            </div>
+        </div>
+        
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+    header{
+        border-bottom: 1px solid black;
+    }
+    .header-top{
+     height: 20px;
+     background-color: #000;   
+    }
+    .header-bottom{
+        height: 120px;
+        display: flex;
+
+    }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div id="products">
+            Content Goes Here
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Main"
+}
+</script>
+
+<style lang="scss" scoped>
+    #products{
+        height: 120px;
+        background-color: #000;
+        color: #fff;
+        font-size: 1.2em;
+        display: flex;
+        align-items: center;
+    }
+</style>
