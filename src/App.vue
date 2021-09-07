56b820d4a9d4748b1ec1002c22536ec7f629d502
Riki420 <girottiriccardo1998@gmail.com>
2021-09-07 17:35:43 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
-@import "@/assets/scss/style.scss";
+@import "./assets/scss/style.scss";
  
   
   .row{
